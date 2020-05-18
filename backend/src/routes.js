045004774df71c75e4407ccc6a2ea3a807768994
MarlonMascarenhas <mmascarenhs@gmail.com>
2020-05-18 11:31:08 -0300

    backend para random foods,

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const connection = require('./database/connection');
+
+const routes = express.Router();
+
+routes.post('/comidas', async (request, response) => {
+    const { name, descricao, img, ingredientes, video }  = request.body;
+
+    await connection('comidas').insert({
+        name,
+        descricao,
+        img,
+        ingredientes,
+        video
+    })
+    return response.json({retorno: 'connection successful'})
+})
+
+
+module.exports = routes;

09f5417c4064054ba899ca1b04b5f2c3f334a0a7
MarlonMascarenhas <mmascarenhs@gmail.com>
2020-05-20 10:16:16 -0300

    random Seleciton funcionando, backend Finalizado

--- backend/src/controllers/RandomController.js
+++ backend/src/controllers/RandomController.js
@@ -2,6 +2,11 @@ const connection = require('../database/connection');
 
 module.exports = {
     async randomSelection(request, response){
+        const amount = await (await connection('comidas')).length
+        const num = Math.floor(Math.random() * amount + 1);
+
+        const food = await connection('comidas').where('id', num);
         
+        return response.json(food);
     }
 }

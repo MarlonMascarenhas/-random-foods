077ae8104bb243a6f48ff735aa4d821c1fc47c78
MarlonMascarenhas <mmascarenhs@gmail.com>
2020-05-20 12:10:18 -0300

    react + bootstrap funcionando, inicio da pagina pronto

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 import './styles.css';
 
 export default function HomePage() {
+    function randomFood(){
+        console.log('hello world');
+    }
+
     return (
-        <h1> hello world</h1>
+        <div className="box-randomFood">
+            <div className="randomFood">
+                <h1>Com fome?</h1>
+                <p>Faça uma refeição aleatória clicando no botão abaixo</p>
+                <Button onClick={randomFood} variant="primary" size="lg">Pegar Refeição</Button>
+            </div>
+        </div>
+        
     )
 }
